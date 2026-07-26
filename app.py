3ed2c1a406be2cacb6d51999dca21003aa4cc58f
tadiparthis <34489011+tadiparthis@users.noreply.github.com>
2026-07-26 10:08:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -359,12 +359,13 @@ else:
     
     st.markdown("""
         <div class="big-announcement">
-            🎉 SURPRISE! WE ARE HAVING A BABY! 👶<br>
+            🎉  WE ARE HAVING A BABY! 👶<br>
             <span style="font-size: 18px; color: #111111; font-weight: bold;">
-                You are going to be <b>DADA & DADI / NANA & NANI</b>! ❤️
+                You are going to be <b>Great GrandPa & Great GrandMa<br> 
+                GrandMA & GrandPa<b> Atta & Mamu <b> Pinni -MS & Pinni MBA <b> </b>! ❤️
             </span><br>
             <span style="font-size: 15px; color: #222222; font-weight: 600;">
-                Expected Arrival: [Insert Due Date Here]
+                Expected Arrival: March, 2027
             </span>
         </div>
     """, unsafe_allow_html=True)
